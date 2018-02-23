d3656a95254287634784241493599d3c9ae51692
Anndo Ko <handcuffanndo@gmail.com>
2018-02-23 13:03:24 -0500

    get name, title, email by crawling the Contact Details pages

--- hw7_part1.py
+++ hw7_part1.py
@@ -22,6 +22,7 @@ def make_request_using_cache(url):
     unique_ident = get_unique_key(url)
 
     if unique_ident in CACHE_DICTION:
+        # access the existing data
         print("Getting cached data...")
         return CACHE_DICTION[unique_ident]
     else:
@@ -29,7 +30,7 @@ def make_request_using_cache(url):
         # make the request and cache the new data
         resp = requests.get(url, headers=header)
         CACHE_DICTION[unique_ident] = resp.text # only store the html
-        dumped_json_cache = json.dumps(CACHE_DICTION) # use the json file for the dic (we're not dealing with json but html)
+        dumped_json_cache = json.dumps(CACHE_DICTION)
         fw = open(CACHE_FNAME,"w")
         fw.write(dumped_json_cache)
         fw.close() # Close the open file
@@ -38,8 +39,8 @@ def make_request_using_cache(url):
 def get_umsi_data(page):
     #### Implement your function here ####
     # form the link
-    baseurl = "https://www.si.umich.edu/directory?rid=All"
-    page = "&page=" + str(page)
+    baseurl = "https://www.si.umich.edu"
+    page = "/directory?rid=All&page=" + str(page)
     url = baseurl + page
 
     # scrap the webpage using BeautifulSoup
@@ -49,21 +50,32 @@ def get_umsi_data(page):
     # create an empty dictionary to store the data
     umsi_titles = {}
 
-    # get the info: name, title, email
+    # get the Contact Detail links
     content_div = page_soup.find(class_ = "view-content")
+    links_items = content_div.find_all(class_ = "field-name-contact-details")
+    for item in links_items:
+        url_node = item.find("a")["href"] # get the node
+        details_url = baseurl + url_node # form the link
+        details_page_text = make_request_using_cache(details_url)
+        details_page_soup = BeautifulSoup(details_page_text, "html.parser")
 
+        # get the name
+        name_section = details_page_soup.find(class_ = "field-name-title")
+        name = name_section.find("h2").text
+        print(name)
 
-    name_items = content_div.find_all(class_ = "field-name-title")
-    title_items = content_div.find_all(class_ = "field-item even")
+        # get the title
+        title_section = details_page_soup.find(class_ = "field-name-field-person-titles")
+        title = title_section.find(class_ = "field-item even").text
+        print(title)
 
-    for item in name_items:
-        name = item.find("h2").text
-        umsi_titles[name] = {}
-
-    print(umsi_titles)
+        # get the email
+        email_secntion = details_page_soup.find(class_ = "field-name-field-person-email")
+        email = email_secntion.find("a")["href"]
+        print(email)
 
 #### Execute funciton, get_umsi_data, here ####
-get_umsi_data(1)
+get_umsi_data(2)
 
 
 
